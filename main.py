8f060e2c1f30b699eda39344b2707835d5d70530
Andrea Ceron <andrea.ceron@studenti.unitn.it>
2024-11-04 08:36:42 +0100

    Stability update

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import torch.nn as nn
 from torchvision.models import resnet50, ResNet50_Weights
 import torch.optim as optim
 from torch.utils.data import DataLoader, random_split
-from utils.dataset_augmentations import createDatasetTest
+from utils.dataset_augmentations import createDataset
 from transformers import CLIPModel
 import yaml
-import pandas
+import pandas as pd
 from networks import create_architecture, load_weights
+import torch.multiprocessing as mp
 
 activations = {}
 torch.manual_seed(42)
@@ -26,17 +27,17 @@ def get_activation(name):
         activations[name] = output
     return hook
 
-def runnig_tests(input_csv, device, in_folder, batch_size = 1, N=10):
-    table = pandas.read_csv(input_csv)[['filename',]]
-    dataset = createDatasetTest(in_folder)
+def runnig_tests(input_csv, device, in_folder, weights_dir, batch_size = 1, N=10):
+    table = pd.DataFrame(columns=['filename','clip'])
+    dataset = createDataset(in_folder,train=False)
     train_dataset, test_dataset = random_split(dataset, [N, len(dataset) - N])
     
     print("Training images: ",len(train_dataset)," - Test images: ",len(test_dataset))
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
 
-    _, model_path, arch, norm_type, patch_size = get_config("clipdet_latent10k_plus", weights_dir="../ClipBased-SyntheticImageDetection/weights")
+    _, model_path, arch, norm_type, patch_size = get_config("clipdet_latent10k_plus", weights_dir)
     model = load_weights(create_architecture(arch), model_path)
     model = model.to(device).eval()
 
@@ -51,24 +52,26 @@ def runnig_tests(input_csv, device, in_folder, batch_size = 1, N=10):
             total_images = torch.squeeze(total_images)
     
             out_tens = model(total_images.clone().to(device)).cpu().numpy()
-            print(out_tens.flatten())
+            
             for ii, logit in zip(np.append(real_image_path, tot_synthetic_image_path), out_tens.flatten()):
-                print(ii,logit)
-                table.loc[ii, 'clip'] = logit
+                entry = pd.DataFrame.from_dict({
+                    "filename": [ii],
+                    "clip":  [logit]
+                })
+                table = pd.concat([table, entry], ignore_index=True)
 
-    # save
     print(table)
     return table
 
 
 if __name__ == "__main__":
-    
+    mp.set_start_method('spawn')
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--aug_folder"     , '-a', type=str, help="The path of the dataset folder with the folders of the images' origin", default="../ClipBased-SyntheticImageDetection/data/synthbuster")
-    parser.add_argument("--in_csv"     , '-i', type=str, help="The path of the input csv file with the list of images", default="../ClipBased-SyntheticImageDetection/data/commercial_tools.csv")
+    parser.add_argument("--aug_folder"     , '-a', type=str, help="The path of the dataset folder with the folders of the images' origin", default="../../Baseline/Semantic-Modification-Detection/data/synthbuster")
+    parser.add_argument("--in_csv"     , '-i', type=str, help="The path of the input csv file with the list of images", default="../../Baseline/Semantic-Modification-Detection/data/commercial_tools.csv")
     parser.add_argument("--out_csv"    , '-o', type=str, help="The path of the output csv file", default="./out.csv")
-    parser.add_argument("--weights_dir", '-w', type=str, help="The directory to the networks weights", default="../ClipBased-SyntheticImageDetection/weights")
+    parser.add_argument("--weights_dir", '-w', type=str, help="The directory to the networks weights", default="../../Baseline/Semantic-Modification-Detection/weights")
     parser.add_argument("--models"     , '-m', type=str, help="List of models to test", default='clipdet_latent10k_plus')
     parser.add_argument("--fusion"     , '-f', type=str, help="Fusion function", default='soft_or_prob')
     parser.add_argument("--device"     , '-d', type=str, help="Torch device", default='cuda:0')
@@ -79,6 +82,10 @@ if __name__ == "__main__":
     else:
         args['models'] = args['models'].split(',')
     
-    runnig_tests(args['in_csv'], args['device'], args['aug_folder'])
+    table = runnig_tests(args['in_csv'], args['device'], args['aug_folder'], args['weights_dir'])
     
-    print("Training completed")
+    output_csv = args['out_csv']
+    os.makedirs(os.path.dirname(os.path.abspath(output_csv)), exist_ok=True)
+    table.to_csv(output_csv, index=False)  # save the results as csv file
+
+    print("Test completed")
--- utils/dataset_augmentations.py
+++ utils/dataset_augmentations.py
@@ -6,61 +6,57 @@ from PIL import Image
 import random
 import cv2
 import numpy as np
+import torch.nn as nn
 from torchvision.transforms import v2
 from torchsr.models import ninasr_b1
 from torchvision import transforms as T
 import torchvision.transforms.functional as F
 import torch
 import io
+import kornia.augmentation as K
+import kornia.enhance as E
+import kornia.utils as U
+import kornia.geometry.transform as TR
 
-class MedianBlurTransform:
-    def __init__(self, ksize=3):
-        self.ksize = ksize 
-
-    def __call__(self, img):
-        img = np.array(img)
-        img = cv2.medianBlur(img, self.ksize)
-        return Image.fromarray(img)
-
-class BoxBlurTransform:
-    def __init__(self, ksize=(3, 3)):
-        self.ksize = ksize 
-
-    def __call__(self, img):
-        img = np.array(img)
-        img = cv2.blur(img, self.ksize)
-        return Image.fromarray(img)
+U.get_cuda_device_if_available(index=0)
 
 class SuperResolutionTransform:
     def __init__(self, upscale_factor=3, device='cuda'):
         self.model = ninasr_b1(pretrained=True, scale=upscale_factor).to(device)
-        self.model.eval()  
+        self.model.eval()
         self.device = device
-        self.upscale_factor = upscale_factor
 
-    def __call__(self, img):
-        img_tensor = T.ToTensor()(img).unsqueeze(0).to(self.device)
-
+    def __call__(self, img_tensor):
         with torch.no_grad():
             sr_img_tensor = self.model(img_tensor)
+        return sr_img_tensor
 
-        sr_img = T.ToPILImage()(sr_img_tensor.squeeze(0).cpu())
-        
-        return sr_img
+
+class SuperResolutionTransform:
+    def __init__(self, upscale_factor=3, device='cuda'):
+        self.model = ninasr_b1(pretrained=True, scale=upscale_factor).to(device)
+        self.model.eval()
+        self.device = device
+
+    def __call__(self, img_tensor):
+        with torch.no_grad():
+            sr_img_tensor = self.model(img_tensor.unsqueeze(0))
+        return sr_img_tensor.squeeze(0)
 
 class JPEGCompressionTransform:
     def __init__(self, quality=100):
         self.quality = quality
 
-    def __call__(self, img):
-        if isinstance(img, torch.Tensor):
-            img = F.to_pil_image(img)
-        
+    def __call__(self, img_tensor):
+        img_tensor = img_tensor.cpu()
+        img_pil = transforms.ToPILImage()(img_tensor)
         with io.BytesIO() as buffer:
-            img.save(buffer, format="JPEG", quality=self.quality)
+            img_pil.save(buffer, format="JPEG", quality=self.quality)
+            buffer.seek(0)
             compressed_img = Image.open(buffer).convert('RGB')
+        compressed_tensor = transforms.ToTensor()(compressed_img).to("cuda:0")
+        return compressed_tensor
 
-        return compressed_img
 
 class GammaCorrectionTransform:
     def __init__(self, gamma=1.0, gain=1.0):
@@ -77,58 +73,51 @@ class GammaCorrectionTransform:
         else:
             raise TypeError(f"Unsupported image type: {type(img)}")
 
-class GrayscaleToRgb:
-    def __call__(self, img):
-        return img.convert('RGB')
-
 class WhiteDarkCorrectionTransform:
     def __init__(self, low_percentile=1, high_percentile=99):
         self.low_percentile = low_percentile
         self.high_percentile = high_percentile
 
-    def __call__(self, img):
-        img_tensor = F.to_tensor(img) if isinstance(img, Image.Image) else img
-
+    def __call__(self, img_tensor):
         low_val = torch.quantile(img_tensor, self.low_percentile / 100.0)
         high_val = torch.quantile(img_tensor, self.high_percentile / 100.0)
 
         img_tensor = torch.clamp(img_tensor, min=low_val, max=high_val)
         img_tensor = (img_tensor - low_val) / (high_val - low_val) 
 
-        return F.to_pil_image(img_tensor) 
+        return img_tensor
 
 class LightShadowAdjustmentTransform:
-    def __init__(self, clip_limit=2.0, tile_grid_size=(8, 8)):
+    def __init__(self, clip_limit=2.0, grid_size=(8, 8)):
         self.clip_limit = clip_limit
-        self.tile_grid_size = tile_grid_size
+        self.grid_size = grid_size
 
-    def __call__(self, img):
-        img_np = np.array(img) if isinstance(img, Image.Image) else img.numpy().transpose(1, 2, 0)
+    def __call__(self, img_tensor):
+        # Ensure the image is in the range [0, 1]
+        img_tensor = torch.clamp(img_tensor, 0.0, 1.0)
 
-        lab = cv2.cvtColor(img_np, cv2.COLOR_RGB2LAB)
-        l_channel, a_channel, b_channel = cv2.split(lab)
-
-        clahe = cv2.createCLAHE(clipLimit=self.clip_limit, tileGridSize=self.tile_grid_size)
-        l_channel_clahe = clahe.apply(l_channel)
-
-        lab_clahe = cv2.merge((l_channel_clahe, a_channel, b_channel))
-        img_clahe = cv2.cvtColor(lab_clahe, cv2.COLOR_LAB2RGB)
-
-        return Image.fromarray(img_clahe)
+        # Apply CLAHE
+        img_clahe = E.equalize_clahe(
+            img_tensor, 
+            clip_limit=self.clip_limit, 
+            grid_size=self.grid_size, 
+            slow_and_differentiable=False
+        )
+        return img_clahe
 
 augmentations = [
-    v2.RandomEqualize(p=1.0),
-    v2.GaussianBlur(kernel_size=9),
-    MedianBlurTransform(ksize=9),
-    BoxBlurTransform(ksize=(9, 9)),
-    v2.Resize(size=(144,144)),
+    K.RandomEqualize(p=1.0),
+    K.RandomGaussianBlur(kernel_size=(9, 9), sigma=(0.1, 2.0), p=1.0),
+    K.RandomMedianBlur((9, 9), p=1.0),
+    K.RandomBoxBlur((9, 9), p=1.0),
+    K.Resize(size=(144,144), p=1.0),
     SuperResolutionTransform(upscale_factor=3),
-    transforms.Compose([v2.Grayscale(), GrayscaleToRgb()]),
+    K.RandomGrayscale(p=1.0),
     JPEGCompressionTransform(quality=5),
-    v2.RandomAdjustSharpness(sharpness_factor=5,p=1.0),
-    GammaCorrectionTransform(gamma=1.0,gain=1.0),
+    K.RandomSharpness(sharpness=1,p=1.0),
+    K.RandomGamma(gamma=(1.5, 1.5), gain=(1.5, 1.5), p=1.0),
     WhiteDarkCorrectionTransform(low_percentile=10, high_percentile=90),
-    LightShadowAdjustmentTransform(clip_limit=2.0, tile_grid_size=(8, 8))
+    LightShadowAdjustmentTransform(clip_limit=2.0, grid_size=(8, 8))
 ]
 
 captions = [
@@ -148,69 +137,14 @@ captions = [
 ]
 
 class ImageDataset(datasets.ImageFolder):
-    def __init__(self, root, transform = None, augmentations=None):
-        super().__init__(root, transform = transform)
-        self.augmentations = augmentations
-        self.imgs = self.samples  
-        self.real_folder = os.path.join(root, 'real_RAISE_1k')
-        self.synthetic_folders = [
-            os.path.join(root, 'dalle2'),
-            os.path.join(root, 'dalle3'),
-            os.path.join(root, 'midjourney-v5'),
-            os.path.join(root, 'firefly')
-        ]
-
-        self.real_images = [ f for f in os.listdir(self.real_folder) if f.endswith('.png') ]
-
-    def __len__(self):
-        return len(self.real_images)
-
-    def __getitem__(self, index):
-        real_image_name = self.real_images[index]
-        real_image_path = os.path.join(self.real_folder, real_image_name)
-
-        real_image = Image.open(real_image_path).convert('RGB')
-        real_images = [real_image]
-
-        if self.augmentations:
-            for aug in self.augmentations:
-                real_images.append(aug(real_image))
-        if self.transform:
-            for i in range(len(real_images)):
-                real_images[i] = self.transform(real_images[i])
-
-        synthetic_folder = random.choice(self.synthetic_folders)
-        synthetic_image_path = os.path.join(synthetic_folder, real_image_name)
-
-        synthetic_image = Image.open(synthetic_image_path).convert('RGB')
-        synthetic_images = [synthetic_image]
-
-        if self.augmentations:
-            for aug in self.augmentations:
-                synthetic_images.append(aug(synthetic_image))
-        if self.transform:
-            for i in range(len(synthetic_images)):
-                synthetic_images[i] = self.transform(synthetic_images[i])
-
-        return torch.stack(real_images), torch.stack(synthetic_images)
-
-def createDataset(rootdataset):
-    transform = transforms.Compose([transforms.Resize(224, interpolation=transforms.InterpolationMode.BICUBIC),
-                                        transforms.CenterCrop((224, 224)),
-                                        processing.make_normalize("clip")])
-    dataset = ImageDataset(root=rootdataset, transform=transform, augmentations=augmentations)
-
-    return dataset
-
-#-----------------------
-
-class ImageDatasetTest(datasets.ImageFolder):
-    def __init__(self, root, transform = None, augmentations=None):
+    def __init__(self, root, transform = None, augmentations=None, device="cuda:0", train=False):
         super().__init__(root, transform = transform)
         self.augmentations = augmentations
         self.imgs = self.samples  
         self.real_folder = os.path.join(root, 'real_RAISE_1k')
         self.root = root
+        self.device = device
+        self.train = train
         self.synthetic_folders = [
             'dalle2',
             'dalle3',
@@ -228,26 +162,36 @@ class ImageDatasetTest(datasets.ImageFolder):
         real_image_path = os.path.join(self.real_folder, real_image_name)
 
         real_image = Image.open(real_image_path).convert('RGB')
+        real_image = transforms.ToTensor()(real_image).to(self.device)
         real_images = [real_image]
         tot_real_entries = []
 
         if self.augmentations:
             for aug in self.augmentations:
                 real_images.append(aug(real_image))
+
+        #show_images_with_captions([img.cpu() for img in real_images],captions)
+
         if self.transform:
             for i in range(len(real_images)):
                 real_images[i] = self.transform(real_images[i])
                 tot_real_entries.append("synthbuster/real_RAISE_1k/"+real_image_name)
 
-        tot_synthetic_images = torch.Tensor([])
+        tot_synthetic_images = torch.Tensor([]).to(self.device)
         tot_fake_entries = []
-        for synthetic_type in self.synthetic_folders:
+
+        tmp = self.synthetic_folders.copy()
+        if(self.train):
+            tmp = [random.choice(tmp)]
+
+        for synthetic_type in tmp:
             synthetic_folder = os.path.join(self.root, synthetic_type)
             synthetic_image_path = os.path.join(synthetic_folder, real_image_name)
 
             synthetic_image = Image.open(synthetic_image_path).convert('RGB')
+            synthetic_image = transforms.ToTensor()(synthetic_image).to(self.device)
             synthetic_images = [synthetic_image]
-
+                                                                                                            
             if self.augmentations:
                 for aug in self.augmentations:
                     synthetic_images.append(aug(synthetic_image))
@@ -260,11 +204,11 @@ class ImageDatasetTest(datasets.ImageFolder):
 
         return torch.stack(real_images), tot_synthetic_images, tot_real_entries, tot_fake_entries
 
-def createDatasetTest(rootdataset):
-    transform = transforms.Compose([transforms.Resize(224, interpolation=transforms.InterpolationMode.BICUBIC),
-                                        transforms.CenterCrop((224, 224)),
-                                        processing.make_normalize("clip")])
-    dataset = ImageDatasetTest(root=rootdataset, transform=transform, augmentations=augmentations)
+def createDataset(rootdataset, device="cuda:0", train=False):
+    transform = torch.nn.Sequential(TR.Resize((224, 224), interpolation='bicubic'),
+                                        K.CenterCrop((224, 224)),
+                                        E.Normalize(mean=(0.48145466, 0.4578275, 0.40821073),std=(0.26862954, 0.26130258, 0.27577711)))
+    dataset = ImageDataset(root=rootdataset, transform=transform, augmentations=augmentations, device=device, train=train)
 
     return dataset
 
@@ -273,15 +217,17 @@ def createDatasetTest(rootdataset):
 def show_images_with_captions(images, captions, nrow=4):
     import matplotlib.pyplot as plt
     
-    fig, axes = plt.subplots(nrow, (len(images) + nrow - 1) // nrow, figsize=(10, 10))
+    fig = plt.figure(figsize=(50, 50))
+    axes = fig.subplots(nrow, (len(images) + nrow - 1) // nrow)
     fig.tight_layout()
     axes = axes.flatten()  
 
     for i, (img, caption) in enumerate(zip(images, captions)):
-        
+        if(img.dim() == 4):
+            img = img.squeeze(0)
         img = img.permute(1, 2, 0).numpy()  
         axes[i].imshow(img)
         axes[i].set_title(caption)
         axes[i].axis('off') 
 
-    plt.show()
+    fig.savefig("real.png")
